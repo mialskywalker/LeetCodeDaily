b46952d64a52cafaeb5bd2f7e20afef99c48db70
mialskywalker <53373335+mialskywalker@users.noreply.github.com>
2024-07-24 20:50:55 +0200

    24.07

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -1,74 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-	string s = "MCMXCIV";
-	int sumOfNums = 0;
+	vector<string> strs{ 
+		"flower", "flow", "flight"
+	};
 
-	for (int i = 0; i < s.length(); i++)
-	{
-		char current = s[i];
 
-		switch (current)
-		{
-		case 'I':
-			if (i + 1 < s.length()) {
-				if (s[i + 1] == 'V' || s[i + 1] == 'X') {
-					sumOfNums -= 1;
-				}
-				else {
-					sumOfNums += 1;
-				}
-			}
-			else {
-				sumOfNums += 1;
-			}
-			break;
-		case 'V':
-			sumOfNums += 5;
-			break;
-		case 'X':
-			if (i + 1 < s.length()) {
-				if (s[i + 1] == 'L' || s[i + 1] == 'C') {
-					sumOfNums -= 10;
-				}
-				else {
-					sumOfNums += 10;
-				}
-			}
-			else {
-				sumOfNums += 10;
-			}
-			break;
-		case 'L':
-			sumOfNums += 50;
-			break;
-		case 'C':
-			if (i + 1 < s.length()) {
-				if (s[i + 1] == 'D' || s[i + 1] == 'M') {
-					sumOfNums -= 100;
-				}
-				else {
-					sumOfNums += 100;
-				}
-			}
-			else {
-				sumOfNums += 100;
-			}
-			break;
-		case 'D':
-			sumOfNums += 500;
-			break;
-		case 'M':
-			sumOfNums += 1000;
-			break;
-		default:
-			break;
-		}
-	}
 
-	return sumOfNums;
+	
 }
